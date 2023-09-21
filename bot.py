e7cc14c916de13a697ea02d987f6d8ff0dc13e90
Geodez_y <geodez_y@bk.ru>
2023-09-21 21:21:08 +0300

    1

--- bot.py
+++ bot.py
@@ -1,31 +1,46 @@
 from aiogram import Bot, Dispatcher, types, html, F, Router
 from aiogram.filters import Command, CommandObject
-from aiogram.types import Message, WebAppInfo, WebAppData
+from aiogram.types import Message, WebAppInfo, WebAppData,  MenuButtonWebApp, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 import asyncio
 import os
 import logging
+from settings import TOKEN
+from handlers import start
 
-TOKEN = '6191110636:AAEisNjXvHHNyiGCpRE6gRfGaQTN2nV8oK4'
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 logging.basicConfig(level=logging.DEBUG)
 
 
-@dp.message(Command("start"))
-async def start(message: Message):
-    message_text = '1234'
-    # builder = ReplyKeyboardBuilder()
-    # builder.row(types.KeyboardButton(
-    #     text="Web",
-    #     web_app=WebAppInfo(url='https://geodeziy.github.io/W-0723/templates/index.html'))
-    # )
-    # await message.answer(message_text, parse_mode="HTML", reply_markup=builder.as_markup())
-    await message.answer(message_text)
+
+@dp.message(Command("webview"))
+async def command_webview(message: Message):
+    await message.answer(
+        "Good. Now you can try to send it via Webview",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text="Web", web_app=WebAppInfo(url="https://geodeziy.github.io/W-0723/templates/index.html")
+                    )
+                ]
+            ]
+        ),
+    )
+
+
+# @dp.message(lambda x: x.web_app_data != None)
+@dp.message(F.web_app_data)
+async def appdata(message: Message):
+    await message.answer(message.web_app_data.data)
+
 
 async def main():
+    dp.include_router(start.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ handlers/start.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, types, html, F, Router
+from aiogram.filters import Command, CommandObject
+from aiogram.types import Message, WebAppInfo, WebAppData,  MenuButtonWebApp, InlineKeyboardMarkup, InlineKeyboardButton
+from filters.chat_type import ChatTypeFilter
+
+
+router = Router()
+router.message.filter(
+    ChatTypeFilter(chat_type=["private"])
+)
+
+
+@router.message(Command("start"))
+async def command_start(message: Message, bot: Bot):
+    await bot.set_chat_menu_button(
+        chat_id=message.chat.id,
+        menu_button=MenuButtonWebApp(text="Web", web_app=WebAppInfo(url="https://geodeziy.github.io/W-0723/templates/index.html")),
+    )
+    await message.answer("""/webview""")
